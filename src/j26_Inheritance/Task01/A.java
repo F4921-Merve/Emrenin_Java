9273a1c0178dc7906a95d61ce6ed1ed2142c5f23
merve turel <merveturel@gmail.com>
2022-11-08 15:01:21 +0300

    Task

---
+++ src/j26_Inheritance/Task01/A.java
@@ -0,0 +1,8 @@
+package j26_Inheritance.Task01;
+
+public class A {
+
+    static  String mesaj=("agam a classdan hörmetler");
+
+
+}
---
+++ src/j26_Inheritance/Task01/B.java
@@ -0,0 +1,9 @@
+package j26_Inheritance.Task01;
+
+public class B extends A {
+
+    public B() {
+
+       mesaj=("static veraible inheitance da class name olmadan coll edilir");
+    }
+}
